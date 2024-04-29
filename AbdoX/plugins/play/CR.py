e1f67aef428c087fb5622e6f7fa12956bf570dad
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-30 02:15:14 +0300

    Update CR.py

--- AbdoX/plugins/play/CR.py
+++ AbdoX/plugins/play/CR.py
@@ -78,36 +78,3 @@ async def yas(client, message):
     )
 
 
-@app.on_message(command ["المالك", "مالك"]
-        and Compulsory_subscription(message)
-        and check_group(chat_id)
-    )
-        allAdminsss = bot.get_chat_administrators(chat_id)
-        Admins = [Admin.user for Admin in allAdminsss if Admin.status == "creator"]
-        for user in Admins:
-            user_inf = bot.get_chat(user.id)
-
-            Photo_user = f"https://t.me/{user_inf.username}"
-            ttxtx = f"""- معلومات المالك : 
-✯︙name: ⤿ {user_inf.first_name}
-
-✯︙user : ⤿  @{user_inf.username}
-
-✯︙Bio: ⤿ #{user_inf.bio}"""
-
-        try:
-            bot.send_photo(
-                chat_id,
-                Photo_user,
-                caption=ttxtx,
-                reply_to_message_id=message.id,
-                reply_markup=Get_prerson(name=user_inf.first_name, id=user_inf.id),
-            )
-
-        except:
-            bot.send_message(
-                chat_id,
-                ttxtx,
-                reply_to_message_id=message.id,
-                reply_markup=Get_prerson(name=user_inf.first_name, id=user_inf.id),
-              )
