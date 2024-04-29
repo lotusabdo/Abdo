e46c6383c19228536e4d58e179f0874c788ecb7d
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-29 12:36:06 +0300

    Add files via upload

---
+++ AbdoX/plugins/Yumi/hastag.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup as  BSP
+from AbdoX  import app as Alina
+from pyrogram import filters
+url = "https://all-hashtag.com/library/contents/ajax_generator.php"
+
+@Alina.on_message(filters.command("hashtag"))
+async def hastag(bot, message):
+    global content
+    try:
+        text = message.text.split(' ',1)[1]
+        data = dict(keyword=text, filter="top")
+
+        res = requests.post(url, data).text
+
+        content = BSP(res, 'html.parser').find("div", {"class":"copy-hashtags"}).string
+    except IndexError:
+        return await message.reply_text("**نموونە :\n\n/hashtag Kuridstan**")
+        
+    
+    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{content}</pre>", quote=True)
+    
+mod_name = "Hᴀsʜᴛᴀɢ"
+help= """
+Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʜᴀsʜᴛᴀɢ ɢᴇɴᴇʀᴀᴛᴏʀ ᴡʜɪᴄʜ ᴡɪʟʟ ɢɪᴠᴇ ʏᴏᴜ ᴛʜᴇ ᴛᴏᴘ 𝟹𝟶 ᴀɴᴅ ᴍᴏʀᴇ ʜᴀsʜᴛᴀɢs ʙᴀsᴇᴅ ᴏғғ ᴏғ ᴏɴᴇ ᴋᴇʏᴡᴏʀᴅ sᴇʟᴇᴄᴛɪᴏɴ.
+° /hastag enter word to generate hastag.
+°Exᴀᴍᴘʟᴇ:  /hastag python """
---
+++ AbdoX/plugins/Yumi/password.py
@@ -0,0 +1,21 @@
+import random, os
+from pyrogram import Client, filters, enums 
+from strings.filters import command
+from AbdoX  import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(command(["genpassword", "genpw","پاسوۆرد","باسورد"]))
+async def password(bot, update):
+    message = await update.reply_text(text="** ويت يحب..**")
+    password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
+    if len(update.command) > 1:
+        qw = update.text.split(" ", 1)[1]
+    else:
+        ST = ["5", "7", "6", "9", "10", "12", "14", "8", "13"] 
+        qw = random.choice(ST)
+    limit = int(qw)
+    random_value = "".join(random.sample(password, limit))
+    txt = f"<b>سنووردار:</b> {str(limit)} \n<b>كلمة مرور: </b><code>{random_value}</code>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('𝗔𝗗𝗗 𝗠𝗘', url='https://t.me/IQMCBOT?startgroup=true')]])
+    await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
