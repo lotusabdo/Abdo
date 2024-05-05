f650b00c5a8f2d1e4dea6b9c128a716a58522373
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-05-05 22:25:58 +0300

    Update start.py

--- AbdoX/utils/inline/start.py
+++ AbdoX/utils/inline/start.py
@@ -7,7 +7,11 @@ from AbdoX import app
 def start_panel(_):
     buttons = [
         [
-
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startchannel=true",
+            )
+        ],
+        [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
@@ -22,13 +26,18 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-     
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
-        
+        [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL)
         ],
-        
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?startchannel=true"),
+        ],
     ]
     return buttons
