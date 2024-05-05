6ff644f1d7ec88b951896d31fd4d030c5ea3f52a
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-05-05 22:09:21 +0300

    Update start.py

--- AbdoX/utils/inline/start.py
+++ AbdoX/utils/inline/start.py
@@ -3,13 +3,12 @@ from pyrogram.types import InlineKeyboardButton
 import config
 from AbdoX import app
 
+
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=" لاضافتي ✅", url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url= "https://t.me/jx_xll"),
+
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
@@ -19,18 +18,17 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="اضغط لاضافتي لمجموعتك✅",
+                text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
+     
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL)
+        ],
         
-        [
-            InlineKeyboardButton(text="مطور السورس ", url=f"https://t.me/EU_TM"),
-            InlineKeyboardButton(text="𝐆𝐑𝐎𝐔𝐏", url=f"https://t.me/jx_xll"), 
-        ],
-        [
-            
-            InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=f"https://t.me/l2_2Y") , 
-        ],
     ]
     return buttons
