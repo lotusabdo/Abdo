131b1a96ff7410e0ca6b2819f99db5316eb17c7b
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-06-01 11:35:44 +0300

    Create كشف.py

---
+++ AbdoX/plugins/play/كشف.py
@@ -0,0 +1,60 @@
+import random
+
+from AbdoX import app
+
+from pyrogram import Client, filters
+
+from pyrogram.types import Message
+
+from pyrogram.enums import ChatMemberStatus
+
+
+
+
+
+def Who(m, user_id):
+
+  user = m.chat.get_member(user_id)
+
+  if user.status == ChatMemberStatus.OWNER:
+
+    return "المالك"
+
+  elif user.status == ChatMemberStatus.ADMINISTRATOR:
+
+    return "مشرف"
+
+  elif user.status == ChatMemberStatus.MEMBER:
+
+    return "العضو"
+
+
+
+@app.on_message(filters.command("ك", "") & filters.group)
+
+def jabwa(c, m):
+
+  name = m.reply_to_message.from_user.first_name
+
+  id = m.reply_to_message.from_user.id
+
+  user = m.reply_to_message.from_user.username
+
+  rank = f"{Who(m,m.reply_to_message.from_user.id)}"
+
+  money = random.randint(1, 100)
+
+  Text =f"""
+
+╖الاسم «» {name}
+
+╢الايدي «» {id}
+
+╢اليوزر «» {user}
+
+╢الرتبه «» {rank}
+
+╜سعر الكشف «» {money} جنيه 😂❤️"""
+
+  return m.reply(Text)
+
