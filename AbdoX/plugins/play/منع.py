2cf8cb4bba50841d03c9c662c06669545bdfd796
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-03 22:41:24 +0300

    Update منع.py

--- AbdoX/plugins/play/منع.py
+++ AbdoX/plugins/play/منع.py
@@ -4,7 +4,7 @@ from AbdoX import app
 
 @app.on_message(filters.text)
 async def delete_text(client, message):
-    word_list = ["سكس", "منتاك"]
+    word_list = ["سكس", "كسمك","خول","متناك"]
     if message.text in word_list:
         await client.delete_messages(message.chat.id, message.id)
-        await client.send_message(message.chat.id, f"لاتسب عيب {message.from_user.first_name}")
+        await client.send_message(message.chat.id, f"ممنوع السب هنا {message.from_user.first_name}")
