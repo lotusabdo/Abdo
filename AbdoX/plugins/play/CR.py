2b3e9de462309b08c2b0b41b2ad04fdafe8d7489
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-03 23:29:23 +0300

    Add files via upload

--- AbdoX/plugins/play/CR.py
+++ AbdoX/plugins/play/CR.py
@@ -54,50 +54,7 @@ async def huhh(client: Client, message: Message):
 
 
 
-
-
-@app.on_message(
-    command(["بودا" , "عبدو","مطور السورس"])
-    
-    
-)
-async def yas(client, message):
-    usr = await client.get_chat("II_U_6")
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"𖥻 SoUrCe DeVeLoPer InFoRmaTioN\n\n 𖥻 UsEr : @{usr.username}\n 𖥻 Id : `{usr.id}`\n 𖥻 BiO : {usr.bio}", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
-                ],
-            ]
-        ),
-    )
-
-@app.on_message(
-command(["محمد" , "ميدو","حمو","مبرمج السورس"])
-    
-    
-) 
-async def yas(client, message):
-    usr = await client.get_chat("YeYeYc")
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"𖥻 SoUrCe DeVeLoPer InFoRmaTioN\n\n 𖥻 UsEr : @{usr.username}\n 𖥻 Id : `{usr.id}`\n 𖥻 BiO : {usr.bio}", 
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
-                ],
-            ]
-        ),
-    )
-
-
-@app.on_message(filters.command(["المطور", "《مطور السورس》", "سينزر", "صاحب السورس", "يا يوسف"], ""), group=73) 
+@app.on_message(filters.command(["بودا" , "عبدو","مطور السورس"] ""), group=73) 
 async def deev(client: Client, message: Message):
      user = await client.get_chat(chat_id="II_U_6")
      name = user.first_name
@@ -108,17 +65,46 @@ async def deev(client: Client, message: Message):
      photo = await client.download_media(photo)
      link = f"https://t.me/{message.chat.username}"
      title = message.chat.title if message.chat.title else message.chat.first_name
-     chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
+     chat_title = f"User : @{usr.username} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
      try:
-      await client.send_message(username, f"**هناك شخص بالحاجه اليك عزيزي المطور**\n{chat_title}\nChat Id : `{message.chat.id}`",
+      await client.send_message(username, f"**هناك شخص بالحاجه اليك عزيزي مطور السورس**\n{chat_title}\nChat Id : `{message.chat.id}`",
       reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
      except:
        pass
      await message.reply_photo(
      photo=photo,
-     caption=f"**Developer Name : {name}** \n**ძᥱ᥎ᥣ᥆ρᥱᖇ ᥙ᥉ᥱᖇ ꪀᥲꪔᥱ : @{username}**\n**{bio}**",
+     caption=f"𖥻 SoUrCe DeVeLoPer InFoRmaTioN\n\n 𖥻 UsEr : @{usr.username}\n 𖥻 Id : `{usr.id}`\n 𖥻 BiO : {usr.bio}", 
      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
      try:
        os.remove(photo)
      except:
         pass
+
+
+
+@app.on_message(filters.command(["محمد" , "ميدو","حمو","مبرمج السورس"] ""), group=73) 
+async def deev(client: Client, message: Message):
+     user = await client.get_chat(chat_id="YeYeYc")
+     name = user.first_name
+     username = user.username 
+     bio = user.bio
+     user_id = user.id
+     photo = user.photo.big_file_id
+     photo = await client.download_media(photo)
+     link = f"https://t.me/{message.chat.username}"
+     title = message.chat.title if message.chat.title else message.chat.first_name
+     chat_title = f"User : @{usr.username} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
+     try:
+      await client.send_message(username, f"**هناك شخص بالحاجه اليك عزيزي المبرمج**\n{chat_title}\nChat Id : `{message.chat.id}`",
+      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
+     except:
+       pass
+     await message.reply_photo(
+     photo=photo,
+     caption=f"𖥻 SoUrCe DeVeLoPer InFoRmaTioN\n\n 𖥻 UsEr : @{usr.username}\n 𖥻 Id : `{usr.id}`\n 𖥻 BiO : {usr.bio}", 
+     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
+     try:
+       os.remove(photo)
+     except:
+        pass
+
