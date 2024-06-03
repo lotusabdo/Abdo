e0760004d54c06037d9dce20c095fd50ce088d65
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-03 22:21:24 +0300

    Update CR.py

--- AbdoX/plugins/play/CR.py
+++ AbdoX/plugins/play/CR.py
@@ -99,7 +99,7 @@ async def yas(client, message):
 
 @app.on_message(filters.command(["المطور", "《مطور السورس》", "سينزر", "صاحب السورس", "يا يوسف"], ""), group=73) 
 async def deev(client: Client, message: Message):
-     user = await client.get_chat(chat_id="I5LPL")
+     user = await client.get_chat(chat_id="II_U_6")
      name = user.first_name
      username = user.username 
      bio = user.bio
