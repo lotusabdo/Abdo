1d5b46f3a8ed699c5aa757c49c9d65317c0967dd
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-04 06:39:51 +0300

    Update CR.py

--- AbdoX/plugins/play/CR.py
+++ AbdoX/plugins/play/CR.py
@@ -53,7 +53,7 @@ async def huhh(client: Client, message: Message):
     )
 
 
-@app.on_message(filters.command(["المطور", "《مطور السورس》", "سينزر", "صاحب السورس", "يا يوسف"], ""), group=73) 
+@app.on_message(filters.command(["مطور السورس","بودا","عبدو","فوديكا"], ""), group=73) 
 async def deev(client: Client, message: Message):
      user = await client.get_chat(chat_id="II_U_6")
      name = user.first_name
@@ -72,7 +72,33 @@ async def deev(client: Client, message: Message):
        pass
      await message.reply_photo(
      photo=photo,
-     caption=f"**Developer Name : {name}** \n**ძᥱ᥎ᥣ᥆ρᥱᖇ ᥙ᥉ᥱᖇ ꪀᥲꪔᥱ : @{username}**\n**{bio}**",
+     caption=f"**𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 𝙽𝚊𝚖𝚎 : {name}** \n**𝚍𝚎𝚟 𝚞𝚜𝚎𝚛 𝚗𝚊𝚖𝚎 : @{username}**\n**{bio}**",
+     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
+     try:
+       os.remove(photo)
+     except:
+        pass
+
+@app.on_message(filters.command(["مبرمج السورس","ميدو","حمو","محمد"], ""), group=73) 
+async def deev(client: Client, message: Message):
+     user = await client.get_chat(chat_id="YeYeYc")
+     name = user.first_name
+     username = user.username 
+     bio = user.bio
+     user_id = user.id
+     photo = user.photo.big_file_id
+     photo = await client.download_media(photo)
+     link = f"https://t.me/{message.chat.username}"
+     title = message.chat.title if message.chat.title else message.chat.first_name
+     chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
+     try:
+      await client.send_message(username, f"**هناك شخص بالحاجه اليك عزيزي المطور**\n{chat_title}\nChat Id : `{message.chat.id}`",
+      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
+     except:
+       pass
+     await message.reply_photo(
+     photo=photo,
+     caption=f"**𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 𝙽𝚊𝚖𝚎 : {name}** \n**𝚍𝚎𝚟 𝚞𝚜𝚎𝚛 𝚗𝚊𝚖𝚎 : @{username}**\n**{bio}**",
      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
      try:
        os.remove(photo)
