7573a714c42c841858c3196fadf45804ef69a9da
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-06 08:22:08 +0300

    Update block.py

--- AbdoX/plugins/sudo/block.py
+++ AbdoX/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AbdoX  import app
-from AbdoX .misc import SUDOERS
-from AbdoX .utils.database import add_gban_user, remove_gban_user
-from AbdoX .utils.decorators.language import language
-from AbdoX .utils.extraction import extract_user
+from AbdoX import app
+from AbdoX.misc import SUDOERS
+from AbdoX.utils.database import add_gban_user, remove_gban_user
+from AbdoX.utils.decorators.language import language
+from AbdoX.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
