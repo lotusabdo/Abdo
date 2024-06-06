d11f1d0d79e50bdf3dc2e77298f6f88440df1ae1
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-06 08:35:33 +0300

    Update call.py

--- AbdoX/core/call.py
+++ AbdoX/core/call.py
@@ -556,7 +556,7 @@ class Call(PyTgCalls):
         return str(round(sum(pings) / len(pings), 3))
 
     async def start(self):
-        LOGGER(__name__).info("😋sᴛᴀʀᴛɪɴɢ ᴘʏᴛɢᴄᴀʟʟs ᴄʟɪᴇɴᴛ...\n")
+        LOGGER(__name__).info("🫧sᴛᴀʀᴛɪɴɢ ᴘʏᴛɢᴄᴀʟʟs ᴄʟɪᴇɴᴛ...\n")
         if config.STRING1:
             await self.one.start()
         if config.STRING2:
