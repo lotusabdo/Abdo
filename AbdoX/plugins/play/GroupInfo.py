2d917480b5b3510c5a0b2c722df488a6fa87b8d7
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-12 06:51:44 +0300

    Update GroupInfo.py

--- AbdoX/plugins/play/GroupInfo.py
+++ AbdoX/plugins/play/GroupInfo.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AbdoX import app
 
-@app.on_message(filters.command("ك مجموعه", prefixes=""))
+@app.on_message(filters.command("كشف مجموعه", prefixes=""))
 async def get_group_status(_, message: Message):
     if len(message.command) != 2:
         await message.reply("الرجاء تقديم اسم مستخدم المجموعة. مثال:كشف مجموعه @EU_TM")
