5c7568cb12180f63eabb0aa93ee16471c2783d9e
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-13 23:30:52 +0300

    Update userbotjoin.py

--- AbdoX/utils/decorators/userbotjoin.py
+++ AbdoX/utils/decorators/userbotjoin.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters
-from AbdoX  import YouTube, app
-from AbdoX .misc import SUDOERS
-from AbdoX .utils.database import (
+from AbdoX import YouTube, app
+from AbdoX.misc import SUDOERS
+from AbdoX.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from AbdoX .utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from AbdoX .utils.inline import botplaylist_markup
+from AbdoX.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
