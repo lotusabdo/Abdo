ff7a0b2ecd9073516c3bce5750d92bfd5e719037
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-13 23:33:47 +0300

    Update queue.py

--- AbdoX/utils/stream/queue.py
+++ AbdoX/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AbdoX .misc import db
-from AbdoX .utils.formatters import check_duration, seconds_to_min
+from AbdoX.misc import db
+from AbdoX.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
