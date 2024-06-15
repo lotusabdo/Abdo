c95ce34c7dc90e0d01f07251348f6e90aa33d1cc
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:32:05 +0300

    Update Youtube.py

--- AbdoX/platforms/Youtube.py
+++ AbdoX/platforms/Youtube.py
@@ -11,8 +11,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AbdoX .utils.database import is_on_off
-from AbdoX .utils.formatters import time_to_seconds
+from AbdoX.utils.database import is_on_off
+from AbdoX.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
