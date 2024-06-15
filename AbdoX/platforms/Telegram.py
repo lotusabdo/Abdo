b89e92e6d6e1b1af530f3370309312f313e913c7
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:31:38 +0300

    Update Telegram.py

--- AbdoX/platforms/Telegram.py
+++ AbdoX/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from AbdoX  import app
-from AbdoX .utils.formatters import (
+from AbdoX import app
+from AbdoX.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
