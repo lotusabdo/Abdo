bbd66a11f4d88c9f4ff24b8b4124661e702e2fb4
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:46:39 +0300

    Update logger.py

--- AbdoX/plugins/sudo/logger.py
+++ AbdoX/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AbdoX  import app
-from AbdoX .misc import SUDOERS
-from AbdoX .utils.database import add_off, add_on
-from AbdoX .utils.decorators.language import language
+from AbdoX import app
+from AbdoX.misc import SUDOERS
+from AbdoX.utils.database import add_off, add_on
+from AbdoX.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
