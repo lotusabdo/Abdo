b073c6553e7b9eebdd5afcaf4d71202e9bb2876a
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:40:34 +0300

    Update population.py

--- AbdoX/plugins/Yumi/population.py
+++ AbdoX/plugins/Yumi/population.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import requests
-from AbdoX  import app
+from AbdoX import app
 
 
 @app.on_message(filters.command("population"))
