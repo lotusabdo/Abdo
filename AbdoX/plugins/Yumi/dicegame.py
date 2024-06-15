a63633769702241b8b8e6d1d401d1ac5e8e32659
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:38:37 +0300

    Update dicegame.py

--- AbdoX/plugins/Yumi/dicegame.py
+++ AbdoX/plugins/Yumi/dicegame.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, enums, filters
 #from config import *
 import asyncio
-from AbdoX  import app as app
+from AbdoX import app as app
 
 from pyrogram.handlers import MessageHandler
 
