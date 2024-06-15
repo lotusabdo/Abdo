495a1480c673cc97d0c1c37d4281c8f8ba7180a4
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:47:24 +0300

    Update raid.py

--- AbdoX/plugins/sudo/raid.py
+++ AbdoX/plugins/sudo/raid.py
@@ -2,8 +2,8 @@ import pyrogram
 import time
 from pyrogram import filters
 from pyrogram import Client
-from AbdoX  import app
-from AbdoX .misc import SUDOERS
+from AbdoX import app
+from AbdoX.misc import SUDOERS
 
 # Define the command handler
 @app.on_message(filters.command("raid", prefixes=".")  & SUDOERS)
