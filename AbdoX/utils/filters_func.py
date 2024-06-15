6308e40b4e4c22b10d7f74da7ed09c84f7ee04d2
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:36:05 +0300

    Update filters_func.py

--- AbdoX/utils/filters_func.py
+++ AbdoX/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
-from AbdoX  import app
+from AbdoX import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from AbdoX .utils.msg_types import button_markdown_parser
-from AbdoX .utils.notes_func import NoteFillings
+from AbdoX.utils.msg_types import button_markdown_parser
+from AbdoX.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 
