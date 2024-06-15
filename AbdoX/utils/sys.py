fd20dc7bf4edd5ac8781336bac721944d5463cc1
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:37:33 +0300

    Update sys.py

--- AbdoX/utils/sys.py
+++ AbdoX/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from AbdoX .misc import _boot_
-from AbdoX .utils.formatters import get_readable_time
+from AbdoX.misc import _boot_
+from AbdoX.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
