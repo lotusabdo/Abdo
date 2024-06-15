d836290acc43da131a53159c44ff40b506491939
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:42:50 +0300

    Update seeker.py

--- AbdoX/plugins/misc/seeker.py
+++ AbdoX/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AbdoX .misc import db
-from AbdoX .utils.database import get_active_chats, is_music_playing
+from AbdoX.misc import db
+from AbdoX.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
