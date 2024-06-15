cbfdc7eeacfe24710bc36599fbbe75fe6723af34
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-15 19:43:44 +0300

    Update state.py

--- AbdoX/plugins/misc/state.py
+++ AbdoX/plugins/misc/state.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import pycountry
-from AbdoX  import app 
+from AbdoX import app 
 
 @app.on_message(filters.command("get_states"))
 def get_states(client, message):
