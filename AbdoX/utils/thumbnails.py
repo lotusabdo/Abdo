c1d72ab11fff521ab2ffb4d0be01f619cbe6009a
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-19 08:44:09 +0300

    Update thumbnails.py

--- AbdoX/utils/thumbnails.py
+++ AbdoX/utils/thumbnails.py
@@ -81,11 +81,11 @@ async def get_thumb(videoid):
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"AleXa", fill="white", font=name_font
+            (5, 5), f"WelCoMe To SoUrCe BoDa Music", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
-            "NOW PLAYING",
+            "Abdo PLAYING",
             fill="white",
             stroke_width=2,
             stroke_fill="white",
